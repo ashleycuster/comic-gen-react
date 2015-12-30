7efe8f89e114d23c91c05031f6414c853ee16f2b
Ashley Custer <ashleyfcuster@gmail.com>
2015-12-30 18:13:40 -0500

    reformatted table

---
+++ src/components/dashboard/info.js
@@ -0,0 +1,51 @@
+"use strict";
+
+var React = require('react'); 
+
+var InfoPanel = React.createClass({
+	propTypes: {
+		marginLeft: React.PropTypes.number.isRequired
+	},
+
+	render: function () {
+		return (
+				<div width="1000px" style={{ marginLeft: this.props.marginLeft }}>
+					<h1>Agency Name, Risk#</h1>
+					<table style={{ marginTop: "20px", marginLeft: "20px"}}>
+						<tr>
+							<td style={{width: "500px"}}>Number of endpoints identified</td>
+							<td style={{width: "1000px"}}>###</td>
+						</tr>
+						<tr>
+							<td>Number of endpoints secured</td>
+							<td>###</td>
+						</tr>
+						<tr height="20px">
+							<td colSpan="2"></td>
+						</tr>
+						<tr>
+							<td>Number of users</td>
+							<td>###</td>
+						</tr>
+						<tr height="20px">
+							<td colSpan="2"></td>
+						</tr>
+						<tr>
+							<td>Licenses required</td>
+							<td>###</td>
+						</tr>
+						<tr>
+							<td>Licenses procured</td>
+							<td>###</td>
+						</tr>
+						<tr>
+							<td>Licenses installed</td>
+							<td>###</td>
+						</tr>
+					</table>
+				</div>
+			);
+	}
+});
+
+module.exports = InfoPanel; 
